9c7605d161018e5c2a49fb5547c703c0745f751d
atef5922 <sifat15-5922@diu.edu.bd>
2025-11-29 21:52:46 +0600

    Add Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
        <h1 className='firstclass'>Hello Atef Programmer</h1>
+       <Home />
+
     </div>
   );
 }
