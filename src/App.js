3736cac5985a4229fa74bbe8fa29933b0ce95630
atef5922 <sifat15-5922@diu.edu.bd>
2025-11-30 22:26:19 +0600

    Header Section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Header from './pages/Header'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -8,7 +9,7 @@ import Contact from './pages/Contact'
 function App() {
   return (
     <div className="App">
-       
+       <Header />
        <Home />
        <About />
        <Contact />
