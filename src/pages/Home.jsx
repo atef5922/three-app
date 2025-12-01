07e0200363400b02a446ecb5dc92cfad0511b95b
atef5922 <sifat15-5922@diu.edu.bd>
2025-12-01 23:17:29 +0600

    Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,84 +6,84 @@ const Home = () => {
   return (
 
     <div className='Home'>
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a href="" class="navbar-brand"> <img src="img/logo.png" alt="" /></a>
-          <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#NavbarIcon">
-            <img src="img/bar.png" alt="" />
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <a href="" className="navbar-brand"> <img src="/assets/img/logo.png" alt="" /></a>
+          <button className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#NavbarIcon">
+            <img src="/assets/img/bar.png" alt="" />
           </button>
-          <div class="collapse navbar-collapse" id="NavbarIcon">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item"> <a href="" class="nav-link">Used Cars</a></li>
-              <li class="nav-item"> <a href="" class="nav-link">Auctions</a></li>
-              <li class="nav-item"> <a href="" class="nav-link">New Cars</a></li>
-              <li class="nav-item"> <a href="" class="nav-link">Sell Cars</a></li>
-              <li class="nav-item"> <a href="" class="nav-link">Local Dealers</a></li>
-              <li class="nav-item"> <a href="" class="nav-link">Support</a></li>
+          <div className="collapse navbar-collapse" id="NavbarIcon">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item"> <a href="" className="nav-link">Used Cars</a></li>
+              <li className="nav-item"> <a href="" className="nav-link">Auctions</a></li>
+              <li className="nav-item"> <a href="" className="nav-link">New Cars</a></li>
+              <li className="nav-item"> <a href="" className="nav-link">Sell Cars</a></li>
+              <li className="nav-item"> <a href="" className="nav-link">Local Dealers</a></li>
+              <li className="nav-item"> <a href="" className="nav-link">Support</a></li>
 
-              <li class="nav-item"> <a href="" class="nav-link SignUp">
-                <img src="img/person-fill.svg" alt="" />
+              <li className="nav-item"> <a href="" className="nav-link SignUp">
+                <img src="/assets/img/person-fill.svg" alt="" />
                 Sign up</a></li>
             </ul>
           </div>
         </div>
       </nav>
 
-      <section class="section1">
-        <div class="container">
+      <section className="section1">
+        <div className="container">
           <h1>Say hello to your next <br />
             awesome vehicle</h1>
-          <p class="Section1P">Featuring Used, Wholesale and Salvage Cars, Trucks  <br /> & SUVs for Sale</p>
+          <p className="Section1P">Featuring Used, Wholesale and Salvage Cars, Trucks  <br /> & SUVs for Sale</p>
 
-          <div class="TopFooterSection">
-            <div class="row">
-              <div class="col-md-2 BorderRight">
+          <div className="TopFooterSection">
+            <div className="row">
+              <div className="col-md-2 BorderRight">
                 <p> Type</p>
-                <select class="form-select">
+                <select className="form-select">
                   <option selected>Used Card</option>
                   <option value="1">1 Card</option>
                   <option value="2">2 Card</option>
                   <option value="3">3 Card</option>
                 </select>
               </div>
-              <div class="col-md-2 BorderRight">
+              <div className="col-md-2 BorderRight">
                 <p>Make</p>
-                <select class="form-select">
+                <select className="form-select">
                   <option selected>Infiniti</option>
                   <option value="1">1 Card</option>
                   <option value="2">2 Card</option>
                   <option value="3">3 Card</option>
                 </select>
               </div>
-              <div class="col-md-2 Withoutp BorderRight ">
-                <p class="SomeMargin"></p>
-                <select class="form-select">
+              <div className="col-md-2 Withoutp BorderRight ">
+                <p className="SomeMargin"></p>
+                <select className="form-select">
                   <option selected>Year</option>
                   <option value="1">1 Card</option>
                   <option value="2">2 Card</option>
                   <option value="3">3 Card</option>
                 </select>
               </div>
-              <div class="col-md-2 Withoutp BorderRight ">
-                <p class="SomeMargin"></p>
-                <select class="form-select">
+              <div className="col-md-2 Withoutp BorderRight ">
+                <p className="SomeMargin"></p>
+                <select className="form-select">
                   <option selected>Model</option>
                   <option value="1">1 Card</option>
                   <option value="2">2 Card</option>
                   <option value="3">3 Card</option>
                 </select>
               </div>
-              <div class="col-md-2 Withoutp">
+              <div className="col-md-2 Withoutp">
                 <p></p>
-                <select class="form-select">
+                <select className="form-select">
                   <option selected>Price</option>
                   <option value="1">1 Card</option>
                   <option value="2">2 Card</option>
                   <option value="3">3 Card</option>
                 </select>
               </div>
-              <div class="col-md-2 Withoutp text-center">
-                <button class="btn HeaderButton"><img src="img/search (1).svg" alt="" /> Search</button>
+              <div className="col-md-2 Withoutp text-center">
+                <button className="btn HeaderButton"><img src="img/search (1).svg" alt="" /> Search</button>
               </div>
 
             </div>
@@ -92,30 +92,30 @@ const Home = () => {
       </section>
 
 
-      <section class="Section2">
-        <div class="container">
-          <div class="row text-center">
-            <div class="col-md-2">
+      <section className="Section2">
+        <div className="container">
+          <div className="row text-center">
+            <div className="col-md-2">
               <img src="img/1.png" alt="" />
             </div>
 
-            <div class="col-md-2 ">
+            <div className="col-md-2 ">
               <img src="img/3.png" alt="" />
             </div>
 
-            <div class="col-md-2">
+            <div className="col-md-2">
               <img src="img/2.png" alt="" />
             </div>
 
-            <div class="col-md-2">
+            <div className="col-md-2">
               <img src="img/4.png" alt="" />
             </div>
 
-            <div class="col-md-2">
+            <div className="col-md-2">
               <img src="img/5.png" alt="" />
             </div>
 
-            <div class="col-md-2">
+            <div className="col-md-2">
               <img src="img/6.png" alt="" />
             </div>
 
@@ -124,51 +124,51 @@ const Home = () => {
       </section>
 
 
-      <section class="section3">
-        <div class="container">
-          <div class="row">
+      <section className="section3">
+        <div className="container">
+          <div className="row">
             <h3>Trending Near You</h3>
-            <div class="col-md-4">
-              <div class="card">
-                <img src="img/car2.png" alt="" class="card-img-top" />
-                <div class="card-body">
+            <div className="col-md-4">
+              <div className="card">
+                <img src="img/car2.png" alt="" className="card-img-top" />
+                <div className="card-body">
                   <h5>Subaru Forester</h5>
                   <p>11 475 Miles</p>
                   <p>AWD <img src="img/dot.svg" alt="" /> loem ipsom</p>
-                  <div class="btn-group gap-2">
-                    <button class="btn Button1">$21,480</button>
-                    <button class="btn Button2">Buy Now</button>
+                  <div className="btn-group gap-2">
+                    <button className="btn Button1">$21,480</button>
+                    <button className="btn Button2">Buy Now</button>
                   </div>
                 </div>
               </div>
             </div>
 
 
-            <div class="col-md-4">
-              <div class="card">
-                <img src="img/car1.png" alt="" class="card-img-top" />
-                <div class="card-body">
+            <div className="col-md-4">
+              <div className="card">
+                <img src="img/car1.png" alt="" className="card-img-top" />
+                <div className="card-body">
                   <h5>Subaru Forester</h5>
                   <p>11 475 Miles</p>
                   <p>AWD <img src="img/dot.svg" alt="" /> loem ipsom</p>
-                  <div class="btn-group gap-2">
-                    <button class="btn Button1">$21,480</button>
-                    <button class="btn Button2">Buy Now</button>
+                  <div className="btn-group gap-2">
+                    <button className="btn Button1">$21,480</button>
+                    <button className="btn Button2">Buy Now</button>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div class="col-md-4">
-              <div class="card">
-                <img src="img/car2.png" alt="" class="card-img-top" />
-                <div class="card-body">
+            <div className="col-md-4">
+              <div className="card">
+                <img src="img/car2.png" alt="" className="card-img-top" />
+                <div className="card-body">
                   <h5>Subaru Forester</h5>
                   <p>11 475 Miles</p>
                   <p>AWD <img src="img/dot.svg" alt="" /> loem ipsom</p>
-                  <div class="btn-group gap-2">
-                    <button class="btn Button1">$21,480</button>
-                    <button class="btn Button2">Buy Now</button>
+                  <div className="btn-group gap-2">
+                    <button className="btn Button1">$21,480</button>
+                    <button className="btn Button2">Buy Now</button>
                   </div>
                 </div>
               </div>
@@ -180,80 +180,80 @@ const Home = () => {
 
 
 
-      <section class="section4 container">
-        <header class="FindcarHeader">
-          <h2 class="findCar">Find car by model</h2>
-          <p class="ArrowArea">
+      <section className="section4 container">
+        <header className="FindcarHeader">
+          <h2 className="findCar">Find car by model</h2>
+          <p className="ArrowArea">
             1/6
             <span><img src="img/arrowLeft.png" alt="" /></span>
             <span><img src="img/arrowRight.png" alt="" /></span>
           </p>
         </header>
 
-        <section class="CarouselSection d-flex">
+        <section className="CarouselSection d-flex">
 
-          <div class="product text-center">
+          <div className="product text-center">
             <img src="img/car/car1.png" alt="" />
-            <div class="ProDetails">
-              <p class="Sedan">Sedan</p>
-              <button class="btn ProBTN">23,457 vehicles</button>
+            <div className="ProDetails">
+              <p className="Sedan">Sedan</p>
+              <button className="btn ProBTN">23,457 vehicles</button>
             </div>
           </div>
 
-          <div class="product text-center">
+          <div className="product text-center">
             <img src="img/car/car1.png" alt="" />
-            <div class="ProDetails">
-              <p class="Sedan">SUV</p>
-              <button class="btn ProBTN1">23,457 vehicles</button>
+            <div className="ProDetails">
+              <p className="Sedan">SUV</p>
+              <button className="btn ProBTN1">23,457 vehicles</button>
             </div>
           </div>
 
-          <div class="product text-center">
+          <div className="product text-center">
             <img src="img/car/car1.png" alt="" />
-            <div class="ProDetails">
-              <p class="Sedan">SUV</p>
-              <button class="btn ProBTN1">23,457 vehicles</button>
+            <div className="ProDetails">
+              <p className="Sedan">SUV</p>
+              <button className="btn ProBTN1">23,457 vehicles</button>
             </div>
           </div>
 
-          <div class="product text-center">
+          <div className="product text-center">
             <img src="img/car/car1.png" alt="" />
-            <div class="ProDetails">
-              <p class="Sedan">SUV</p>
-              <button class="btn ProBTN1">23,457 vehicles</button>
+            <div className="ProDetails">
+              <p className="Sedan">SUV</p>
+              <button className="btn ProBTN1">23,457 vehicles</button>
             </div>
           </div>
 
-          <div class="product text-center">
+          <div className="product text-center">
 
           </div>
 
-          <div class="product text-center">
+          <div className="product text-center">
             <img src="img/car/car1.png" alt="" />
-            <div class="ProDetails">
-              <p class="Sedan">SUV</p>
-              <button class="btn ProBTN1">23,457 vehicles</button>
+            <div className="ProDetails">
+              <p className="Sedan">SUV</p>
+              <button className="btn ProBTN1">23,457 vehicles</button>
             </div>
           </div>
-          <div class="product text-center">
+          <div className="product text-center">
             <img src="img/car/car1.png" alt="" />
-            <div class="ProDetails">
-              <p class="Sedan">SUV</p>
-              <button class="btn ProBTN1">23,457 vehicles</button>
+            <div className="ProDetails">
+              <p className="Sedan">SUV</p>
+              <button className="btn ProBTN1">23,457 vehicles</button>
             </div>
           </div>
-          <div class="product text-center">
+          <div className="product text-center">
             <img src="img/car/car1.png" alt="" />
-            <div class="ProDetails">
-              <p class="Sedan">SUV</p>
-              <button class="btn ProBTN1">23,457 vehicles</button>
+            <div className="ProDetails">
+              <p className="Sedan">SUV</p>
+              <button className="btn ProBTN1">23,457 vehicles</button>
             </div>
           </div>
-          <div class="product text-center">
+          <div className="product text-center">
             <img src="img/car/car1.png" alt="" />
-            <div class="ProDetails">
-              <p class="Sedan">SUV</p>
-              <button class="btn ProBTN1">23,457 vehicles</button>
+            <div className="ProDetails">
+              <p className="Sedan">SUV</p>
+              <button className="btn ProBTN1">23,457 vehicles</button>
             </div>
           </div>
         </section>
